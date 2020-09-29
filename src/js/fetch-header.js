9c35e4197fa824ce748bae5a5b37a85ad8b16e53
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-29 13:42:06 +0200

    header fetch

---
+++ src/js/fetch-header.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:4000/api/v1/adoptsections/1', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxMzc1MDg1LCJleHAiOjE2MDEzNzg2ODV9.54PkfDXdSo4pM5LV72c0gaHjL81rnFV4jvvPcEbPIjE',
+    },
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (result) {
+      console.log(result.asset.url)
+      document.querySelector('#header-kittens').src = result.asset.url
+    })
+})
