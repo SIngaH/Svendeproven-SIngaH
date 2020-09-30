1e282457c860a7b8c8a57736e2adb42864267476
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-30 10:04:06 +0200

    adopter et dyr kommer nu fra apiet

--- src/js/fetch-adopter-et-dyr.js
+++ src/js/fetch-adopter-et-dyr.js
@@ -1,2 +1,33 @@
-// http://localhost:4000/api/v1/adoptsections/3
-console.log('hihi')
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:4000/api/v1/adoptsections/3', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxMzc1MDg1LCJleHAiOjE2MDEzNzg2ODV9.54PkfDXdSo4pM5LV72c0gaHjL81rnFV4jvvPcEbPIjE',
+    },
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (result) {
+      document.querySelector('#adopter-et-dyr').innerHTML =
+        `
+        <img
+        src="` +
+        result.asset.url +
+        `"
+        alt="` +
+        result.title +
+        `"
+        id="adopter-et-dyr-img"
+        />
+        <h4>` +
+        result.title +
+        `</h4>
+        <p>` +
+        result.content +
+        `</p>
+        `
+    })
+})
