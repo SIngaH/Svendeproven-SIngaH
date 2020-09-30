ae01e2722fc25d8e4a1dca60ca6d2660e28a823b
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-30 08:51:53 +0200

    nu kan man klikke på dyrene på index siden og se det enkelte dyr på en anden side

---
+++ src/js/et-dyr.js
@@ -0,0 +1,46 @@
+/*
+http://localhost:4000/api/v1/animals
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  let params = new URLSearchParams(document.location.search)
+  let id = params.get('id')
+  let main = document.querySelector('main')
+  fetch('http://localhost:4000/api/v1/animals/' + id, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxMzc1MDg1LCJleHAiOjE2MDEzNzg2ODV9.54PkfDXdSo4pM5LV72c0gaHjL81rnFV4jvvPcEbPIjE',
+    },
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (result) {
+      main.innerHTML =
+        `<article>
+            <h3>` +
+        result.name +
+        `</h3>
+            <img src="` +
+        result.asset.url +
+        `" alt="` +
+        result.name +
+        `">
+        <div class="et-dyr-text">
+                <p>` +
+        result.asset.createdAt +
+        ` change later</p>
+                <p>` +
+        result.name +
+        ` is ` +
+        result.age +
+        ` years old</p>
+                <p>` +
+        result.description +
+        `</p>
+            </div>
+        </article>
+        `
+    })
+})
