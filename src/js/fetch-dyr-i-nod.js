ce0c2f6f7a1fdf0b30cd5da3020a685fe0e4da93
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-30 09:36:06 +0200

    dyr i nød bliver nu hentet fra apiet

---
+++ src/js/fetch-dyr-i-nod.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:4000/api/v1/adoptsections/2', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxMzc1MDg1LCJleHAiOjE2MDEzNzg2ODV9.54PkfDXdSo4pM5LV72c0gaHjL81rnFV4jvvPcEbPIjE',
+    },
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (result) {
+      document.querySelector('#dyr-i-nod').innerHTML =
+        `
+        <img src="` +
+        result.asset.url +
+        `" alt="dyr i nød" id="dyr-i-nod-img" />
+        <h4>` +
+        result.title +
+        `</h4>
+        <p>` +
+        result.content +
+        `</p>
+      `
+    })
+})
