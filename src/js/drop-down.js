8402933bed13daab74d7a302cce372625f9d4fcf
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-10-01 08:56:43 +0200

    drop down pilen ændres efter om man kan se ul

--- src/js/drop-down.js
+++ src/js/drop-down.js
@@ -3,6 +3,7 @@ let navUl = document.querySelector('#nav ul')
 dropDown.addEventListener('click', () => {
   if (navUl.style.display === 'block') {
     navUl.style.display = 'none'
+    dropDown.style.transform = 'rotate(-90deg)'
   } else {
     dropDown.style.transform = 'rotate(0deg)'
     navUl.style.display = 'block'
