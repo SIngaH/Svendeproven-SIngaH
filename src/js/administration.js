07a89e45fae0c561b4c9a0191f2bafd37d2082f8
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-10-05 10:42:26 +0200

    nu kan man også slette fra apiet fra administrationssiden

--- src/js/administration.js
+++ src/js/administration.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let postLI = document.querySelectorAll('.postLi')
   let deleteLI = document.querySelectorAll('.deleteLi')
   let saveUrl = sessionStorage.getItem('saveUrl')
+  let saveNumber = sessionStorage.getItem('saveNumber')
   let currentIndex = sessionStorage.getItem('index')
   let postTitleContent = document.querySelector('#postForm .form-title-content')
   let postTitleContentH3 = document.querySelector(
@@ -12,10 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let postAnimals = document.querySelector('#postForm .form-animals')
   let postAnimalsH3 = document.querySelector('#postForm .form-animals h3')
   let endpoints = [
-    'http://localhost:4000/api/v1/adoptsections',
-    'http://localhost:4000/api/v1/abouts',
-    'http://localhost:4000/api/v1/volunteers',
-    'http://localhost:4000/api/v1/animals',
+    'http://localhost:4000/api/v1/adoptsections/',
+    'http://localhost:4000/api/v1/abouts/',
+    'http://localhost:4000/api/v1/volunteers/',
+    'http://localhost:4000/api/v1/animals/',
   ]
   for (let i = 0; i < postLI.length; i++) {
     postLI[i].addEventListener('click', () => {
@@ -47,19 +48,8 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
   }
-
-  let deleteTitleContentH3 = document.querySelector(
-    '#deleteForm .form-title-content h3'
-  )
-  let deleteTitleContent = document.querySelector(
-    '#deleteForm .form-title-content'
-  )
-  let deleteVolunteers = document.querySelector('#deleteForm .form-volunteers')
-  let deleteVolunteersH3 = document.querySelector(
-    '#deleteForm .form-volunteers h3'
-  )
-  let deleteAnimals = document.querySelector('#deleteForm .form-animals')
-  let deleteAnimalsH3 = document.querySelector('#deleteForm .form-animals h3')
+  let deleteForm = document.querySelector('#deleteForm .form-delete')
+  let deleteFormH3 = document.querySelector('#deleteForm .form-delete h3')
 
   for (let ind = 0; ind < deleteLI.length; ind++) {
     deleteLI[ind].addEventListener('click', () => {
@@ -68,26 +58,15 @@ document.addEventListener('DOMContentLoaded', () => {
       postTitleContent.style.display = 'none'
       postVolunteers.style.display = 'none'
       postAnimals.style.display = 'none'
+      deleteForm.style.display = 'flex'
       if (ind == 0) {
-        deleteTitleContent.style.display = 'flex'
-        deleteTitleContentH3.innerHTML = `Slette i Adopt sektionerne`
-        deleteVolunteers.style.display = 'none'
-        deleteAnimals.style.display = 'none'
+        deleteFormH3.innerHTML = `Slette i Adopt sektionerne`
       } else if (ind == 1) {
-        deleteTitleContent.style.display = 'flex'
-        deleteTitleContentH3.innerHTML = `Slette i Abouts sektionerne`
-        deleteVolunteers.style.display = 'none'
-        deleteAnimals.style.display = 'none'
+        deleteFormH3.innerHTML = `Slette i Abouts sektionerne`
       } else if (ind == 2) {
-        deleteVolunteers.style.display = 'flex'
-        deleteVolunteersH3.innerHTML = `Slette i Volunteers sektionerne`
-        deleteTitleContent.style.display = 'none'
-        deleteAnimals.style.display = 'none'
+        deleteFormH3.innerHTML = `Slette i Volunteers sektionerne`
       } else if (ind == 3) {
-        deleteAnimals.style.display = 'flex'
-        deleteAnimalsH3.innerHTML = `Slette i Animals sektionerne`
-        deleteTitleContent.style.display = 'none'
-        deleteVolunteers.style.display = 'none'
+        deleteFormH3.innerHTML = `Slette i Animals sektionerne`
       }
     })
   }
@@ -262,175 +241,42 @@ document.addEventListener('DOMContentLoaded', () => {
   /*---------------------title and content-----------------------*/
 
   let DelerrorElement = document.querySelector(
-    '#deleteForm .form-title-content .error-message'
+    '#deleteForm .form-delete .error-message'
   )
-  let Deloverskrift = document.querySelector(
-    '#deleteForm .form-title-content .form__overskrift'
-  )
-  let Deltextarea = document.querySelector(
-    '#deleteForm .form-title-content .form__comment'
-  )
-  deleteTitleContent.addEventListener('submit', (e) => {
+  let number = document.querySelector('#deleteForm .form-delete .form__number')
+  deleteForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    /*--------------------------overskrift--------------------------------- */
-    if (Deloverskrift.value === '' || Deloverskrift.value == null) {
-      DelerrorElement.style.display = 'block'
-      DelerrorElement.innerHTML = 'Der mangler en overskrift'
-      Deloverskrift.focus()
-      Deloverskrift.style.border = border
-      return false
-    } else {
-      Deloverskrift.style.border = borderGone
-    }
+    /*-----------------------number-------------------------------------*/
 
-    /*-----------------------textarea-------------------------------------*/
-    if (Deltextarea.value === '' || Deltextarea.value == null) {
+    if (number.value === '' || number.value == null) {
       DelerrorElement.style.display = 'block'
-      DelerrorElement.innerHTML = 'Der mangler brødtext'
-      Deltextarea.focus()
-      Deltextarea.style.border = border
+      DelerrorElement.innerHTML = 'Hvilken sektion vil du slette?'
+      number.focus()
+      number.style.border = border
       return false
     } else {
-      Deltextarea.style.border = borderGone
+      number.style.border = borderGone
+    }
+    if (isNaN(number.value)) {
+      AerrorElement.style.display = 'block'
+      AerrorElement.innerHTML = 'Alderen skal skrives i tal'
+      number.focus()
+      number.style.border = border
+      return false
+    } else {
+      number.style.border = borderGone
     }
 
     DelerrorElement.style.display = 'block'
 
-    if (deleteTitleContentH3.innerText.includes('Adopt')) {
-      DelerrorElement.innerHTML = 'Du har nu fjernet fra Adopt sektionerne'
-    } else if (deleteTitleContentH3.innerText.includes('Abouts')) {
-      DelerrorElement.innerHTML = 'Du har nu fjernet fra Abouts sektionerne'
-    }
-
-    let data = {
-      title: Deloverskrift.value,
-      content: Deltextarea.value,
-    }
-
-    deleter(saveUrl, data)
-    return false
-  })
-  /*---------------------volunteers-----------------------*/
-  let Deltitle = document.querySelector(
-    '#deleteForm .form-volunteers .form__overskrift'
-  )
-  let Delcontent = document.querySelector(
-    '#deleteForm .form-volunteers .form__comment'
-  )
-  let Delerror = document.querySelector(
-    '#deleteForm .form-volunteers .error-message'
-  )
-  deleteVolunteers.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    /*--------------------------overskrift--------------------------------- */
-    if (Deltitle.value === '' || Deltitle.value == null) {
-      Delerror.style.display = 'block'
-      Delerror.innerHTML = 'Der mangler en overskrift'
-      Deltitle.focus()
-      Deltitle.style.border = border
-      return false
-    } else {
-      Deltitle.style.border = borderGone
-    }
-
-    /*-----------------------textarea-------------------------------------*/
-    if (Delcontent.value === '' || Delcontent.value == null) {
-      Delerror.style.display = 'block'
-      Delerror.innerHTML = 'Der mangler brødtext'
-      Delcontent.focus()
-      Delcontent.style.border = border
-      return false
-    } else {
-      Delcontent.style.border = borderGone
-    }
-
-    Delerror.style.display = 'block'
-    Delerror.innerHTML = 'Du har nu fjernet fra Volunteers sektionerne'
-
-    let theIndex = currentIndex++
-    let data = {
-      title: Deltitle.value,
-      content: Delcontent.value,
-      extra: null,
-      AssetId: theIndex,
-    }
-    console.log(data)
-
-    deleter(saveUrl, data)
-    return false
-  })
-
-  /*---------------------animals-----------------------*/
-  let Antitle = document.querySelector('.form-animals .form__overskrift')
-  let Ancontent = document.querySelector('.form-animals .form__comment')
-  let Aage = document.querySelector('.form-animals .form__age')
-  let AnerrorElement = document.querySelector('.form-animals .error-message')
-
-  postAnimals.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    /*--------------------------overskrift--------------------------------- */
-    if (Antitle.value === '' || Antitle.value == null) {
-      AnerrorElement.style.display = 'block'
-      AnerrorElement.innerHTML = 'Der mangler en overskrift'
-      Antitle.focus()
-      Antitle.style.border = border
-      return false
-    } else {
-      Antitle.style.border = borderGone
-    }
-
-    /*--------------------------Age--------------------------------- */
-    if (Aage.value === '' || Aage.value == null) {
-      AnerrorElement.style.display = 'block'
-      AnerrorElement.innerHTML = 'Der mangler en alder'
-      Aage.focus()
-      Aage.style.border = border
-      return false
-    } else {
-      Aage.style.border = borderGone
-    }
-    if (isNaN(Aage.value)) {
-      AnerrorElement.style.display = 'block'
-      AnerrorElement.innerHTML = 'Alderen skal skrives i tal'
-      Aage.focus()
-      Aage.style.border = border
-      return false
-    } else {
-      Aage.style.border = borderGone
-    }
-
-    /*-----------------------textarea-------------------------------------*/
-    if (Ancontent.value === '' || Ancontent.value == null) {
-      AnerrorElement.style.display = 'block'
-      AnerrorElement.innerHTML = 'Der mangler brødtext'
-      Ancontent.focus()
-      Ancontent.style.border = border
-      return false
-    } else {
-      Ancontent.style.border = borderGone
-    }
-
-    AnerrorElement.style.display = 'block'
-    AnerrorElement.innerHTML = 'Du har nu tilføjet til Animals sektionerne'
-
-    let theIndex = currentIndex++
-    let data = {
-      title: Antitle.value,
-      content: Ancontent.value,
-      age: Aage.value,
-      extra: null,
-      AssetId: theIndex,
-    }
-
-    post(saveUrl, data)
+    sessionStorage.setItem('saveNumber', number.value)
+    console.log(saveNumber)
+    deleter(saveUrl, number.value)
     return false
   })
   /* ------------------- fetch post og delete ------------------------- */
-  // let test = { name: 'hannah', email: 'hjkl@jkl.ckl' }
-  // post('http://localhost:4000/api/v1/subscribers', test)
+
   function post(url, fetchData) {
     fetch(url, {
       method: 'POST',
@@ -451,46 +297,26 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Error:', error)
       })
   }
-  // let testData = { name: 'hannah', email: 'hjkl@jkl.ckl' }
-  // deleter('http://localhost:4000/api/v1/subscribers/', testData)
-  function deleter(url, testData) {
-    let thisEmail = testData.email
-    if (thisEmail == null) {
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          token:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxODgyNjI0LCJleHAiOjE2MDE4ODYyMjR9.G1eHRaOqh2373tWPfuCvSLKNOFdv3kMbDhrrJdQmAuo',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxODgyNjI0LCJleHAiOjE2MDE4ODYyMjR9.G1eHRaOqh2373tWPfuCvSLKNOFdv3kMbDhrrJdQmAuo',
-        },
-        body: JSON.stringify(testData),
+
+  function deleter(url, number) {
+    fetch(url + number, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxODg2OTAzLCJleHAiOjE2MDE4OTA1MDN9.iNbxsdQMtq5b5sq-KvToGYedNC1Cud1OsxXjZJgirKI',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxODg2OTAzLCJleHAiOjE2MDE4OTA1MDN9.iNbxsdQMtq5b5sq-KvToGYedNC1Cud1OsxXjZJgirKI',
+        username: 'admin',
+        password: '1234',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data)
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Success:', data)
-        })
-        .catch((error) => {
-          console.error('Error:', error)
-        })
-    } else {
-      fetch(url + thisEmail, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          token:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTUkQy9QMmVlZnpJbFguRTJ2VXFqb0JLZU05dGg2djc5NkpDbzBKQnN5cmtJd090NXJ4WUEuVnEiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA1LTE3VDE5OjI1OjM0LjQwNFoifSwiaWF0IjoxNjAxMzc1MDg1LCJleHAiOjE2MDEzNzg2ODV9.54PkfDXdSo4pM5LV72c0gaHjL81rnFV4jvvPcEbPIjE',
-        },
-        body: JSON.stringify(testData),
+      .catch((error) => {
+        console.error('Error:', error)
       })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Success:', data)
-        })
-        .catch((error) => {
-          console.error('Error:', error)
-        })
-    }
   }
 })
